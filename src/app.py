64740dbb6477020aa165565fe4cf1c6cab176449
Lluis Espert Cuquerella <lluisespertc@gmail.com>
2025-02-17 15:19:59 +0100

    Añadidas nuevas funcionalidades en el dashboard del administrador (Ya se pueden crear tareas, con nombre y descripción y asignárselas a un trabajador)

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ import hashlib
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'lluis'
@@ -28,6 +28,79 @@ def login():
         return jsonify({"message": "Login successful!", "rol": user['rol']}), 200
     else:
         return jsonify({"message": "Invalid credentials!"}), 401
+
+@app.route('/api/todos', methods=['GET', 'POST'])
+def manage_todos():
+    if request.method == 'POST':
+        try:
+            data = request.json
+            label = data['label']
+            description = data.get('description', '')
+            is_done = data.get('is_done', False)
+            user_id = data.get('user_id', None)
+
+            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+            cursor.execute('INSERT INTO todos (label, description, is_done, user_id) VALUES (%s, %s, %s, %s)', (label, description, is_done, user_id))
+            mysql.connection.commit()
+
+            new_todo_id = cursor.lastrowid
+            cursor.execute('''
+                SELECT todos.id, todos.label, todos.description, todos.is_done, users.username AS assigned_user 
+                FROM todos 
+                LEFT JOIN users ON todos.user_id = users.id
+                WHERE todos.id = %s
+            ''', (new_todo_id,))
+            new_todo = cursor.fetchone()
+
+            return jsonify(new_todo), 201
+        except Exception as e:
+            print("Error al insertar datos:", e)
+            return jsonify({"error": "Error al insertar datos"}), 500
+    elif request.method == 'GET':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('''
+            SELECT todos.id, todos.label, todos.description, todos.is_done, users.username AS assigned_user 
+            FROM todos 
+            LEFT JOIN users ON todos.user_id = users.id
+        ''')
+        todos = cursor.fetchall()
+        return jsonify(todos)
+
+@app.route('/api/todos/<int:id>', methods=['PUT', 'DELETE'])
+def modify_todo(id):
+    if request.method == 'PUT':
+        data = request.json
+        label = data['label']
+        description = data['description']
+        is_done = data['is_done']
+        
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('UPDATE todos SET label = %s, description = %s, is_done = %s WHERE id = %s', (label, description, is_done, id))
+        mysql.connection.commit()
+        
+        cursor.execute('''
+            SELECT todos.id, todos.label, todos.description, todos.is_done, users.username AS assigned_user 
+            FROM todos 
+            LEFT JOIN users ON todos.user_id = users.id
+            WHERE todos.id = %s
+        ''', (id,))
+        updated_todo = cursor.fetchone()
+        
+        return jsonify(updated_todo)
+    
+    elif request.method == 'DELETE':
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('DELETE FROM todos WHERE id = %s', (id,))
+        mysql.connection.commit()
+        
+        return '', 204
+
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT id, username FROM users WHERE rol = %s', ('user',))
+    users = cursor.fetchall()
+    return jsonify(users)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,165 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-const Dashboard = ({ username, onLogout }) => {
+const Dashboard = () => {
+  const baseURL = 'http://localhost:5000/api'; 
+
+  const [esEditable, setEsEditable] = useState(false);
+  const [label, setLabel] = useState('');
+  const [description, setDescription] = useState('');
+  const [todos, setTodos] = useState([]);
+  const [usuarios, setUsuarios] = useState([]);
+  const [editarTarea, setEditarTarea] = useState({});
+  const [labelEdit, setLabelEdit] = useState('');
+  const [descriptionEdit, setDescriptionEdit] = useState('');
+  const [completedEdit, setCompletedEdit] = useState(false);
+  const [selectedUser, setSelectedUser] = useState('');
+
+  const handleSubmitAdd = async (event) => {
+    event.preventDefault();
+
+    const dataToSend = {
+      label: label,
+      description: description,
+      is_done: false,
+      user_id: selectedUser
+    };
+
+    try {
+      await axios.post(`${baseURL}/todos`, dataToSend);
+      setLabel('');
+      setDescription('');
+      setSelectedUser('');
+      getTodos();
+    } catch (error) {
+      console.log('error:', error.response.status, error.response.statusText);
+    }
+  };
+
+  const handleEdit = (editarTarea) => {
+    setEsEditable(true);
+    setEditarTarea(editarTarea);
+    setLabelEdit(editarTarea.label);
+    setDescriptionEdit(editarTarea.description);
+    setCompletedEdit(editarTarea.is_done);
+  };
+
+  const handleSubmitEdit = async (event) => {
+    event.preventDefault();
+
+    const dataToSend = {
+      label: labelEdit,
+      description: descriptionEdit,
+      is_done: completedEdit
+    };
+
+    try {
+      await axios.put(`${baseURL}/todos/${editarTarea.id}`, dataToSend);
+      getTodos();
+      setEsEditable(false);
+      setEditarTarea({});
+      setLabelEdit('');
+      setDescriptionEdit('');
+      setCompletedEdit(false);
+    } catch (error) {
+      console.log('error:', error.response.status, error.response.statusText);
+    }
+  };
+
+  const handleDelete = async (tareaId) => {
+    try {
+      await axios.delete(`${baseURL}/todos/${tareaId}`);
+      getTodos();
+    } catch (error) {
+      console.log('error:', error.response.status, error.response.statusText);
+    }
+  };
+
+  const getTodos = async () => {
+    try {
+      const response = await axios.get(`${baseURL}/todos`);
+      setTodos(response.data);
+    } catch (error) {
+      console.log('error:', error.response.status, error.response.statusText);
+    }
+  };
+
+  const getUsuarios = async () => {
+    try {
+      const response = await axios.get(`${baseURL}/users`);
+      setUsuarios(response.data);
+    } catch (error) {
+      console.log('error:', error.response.status, error.response.statusText);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+    getUsuarios();
+  }, []);
+
   return (
-    <div className="dashboard-container">
-      <h1>Welcome, {username}!</h1>
-      <p>This is your  Admin dashboard.</p>
-      <button onClick={onLogout}>Logout</button>
+    <div className="container my-5">
+      <h1 className="text-success">Gestión de tareas</h1>
+      {esEditable ? (
+        <form onSubmit={handleSubmitEdit}>
+          <div className="text-start mb-3">
+            <label htmlFor="labelEdit" className="form-label">Editar Nombre de la Tarea</label>
+            <input type="text" className="form-control" id="labelEdit" value={labelEdit} onChange={(event) => { setLabelEdit(event.target.value) }} />
+          </div>
+          <div className="text-start mb-3">
+            <label htmlFor="descriptionEdit" className="form-label">Editar Descripción de la Tarea</label>
+            <textarea className="form-control" id="descriptionEdit" value={descriptionEdit} onChange={(event) => { setDescriptionEdit(event.target.value) }} />
+          </div>
+          <div className="text-start mb-3 form-check">
+            <input type="checkbox" className="form-check-input" id="exampleCheck1" checked={completedEdit} onChange={(event) => { setCompletedEdit(event.target.checked) }} />
+            <label className="form-check-label" htmlFor="exampleCheck1">Completada la tarea</label>
+          </div>
+          <button type="submit" className="btn btn-primary me-2">Enviar</button>
+          <button type="reset" className="btn btn-secondary">Reset</button>
+        </form>
+      ) : (
+        <form onSubmit={handleSubmitAdd}>
+          <div className="text-start mb-3">
+            <label htmlFor="label" className="form-label">Nombre de la Tarea</label>
+            <input type="text" className="form-control" id="label" value={label} onChange={(event) => setLabel(event.target.value)} />
+          </div>
+          <div className="text-start mb-3">
+            <label htmlFor="description" className="form-label">Descripción de la Tarea</label>
+            <textarea className="form-control" id="description" value={description} onChange={(event) => setDescription(event.target.value)} />
+          </div>
+          <div className="text-start mb-3">
+            <label htmlFor="selectUser" className="form-label">Asignar Usuario</label>
+            <select className="form-select" id="selectUser" value={selectedUser} onChange={(event) => setSelectedUser(event.target.value)}>
+              <option value="">Seleccione un usuario</option>
+              {usuarios.map((usuario) => (
+                <option key={usuario.id} value={usuario.id}>{usuario.username}</option>
+              ))}
+            </select>
+          </div>
+          <button type="submit" className="btn btn-primary">Añadir Tarea</button>
+        </form>
+      )}
+      <h2 className="text-primary mt-5">Lista de tareas</h2>
+      <ul className="text-start list-group">
+        {todos.map((item) => (
+          <li key={item.id} className="list-group-item d-flex justify-content-between">
+            <div>
+              {item.is_done ? <i className="fa fa-thumbs-up text-success me-2"></i> : <i className="fa fa-times-circle text-danger me-2"></i>}
+              {item.label} - {item.description} - Asignado a: {item.assigned_user || "N/A"}
+            </div>
+            <div>
+              <span onClick={() => handleEdit(item)}>
+                <i className="fa fa-check-square-o text-primary me-2"></i>
+              </span>
+              <span onClick={() => handleDelete(item.id)}>
+                <i className="fa fa-remove text-danger"></i>
+              </span>
+            </div>
+          </li>
+        ))}
+        <li className="list-group-item text-end">{todos.length === 0 ? 'No hay tareas, añade nuevas tareas' : todos.length + ' tareas'}</li>
+      </ul>
     </div>
   );
 };
